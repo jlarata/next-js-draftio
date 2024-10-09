247fdded34ae6ad12f9bb2fac494eee50786e1a5
jlarata <arielwdev@gmail.com>
2024-10-09 14:14:03 -0300

    ...

---
+++ app/dashboard/leagues/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <p>Leagues page not implemented</p>
+}
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,10 +1,16 @@
+'use client';
+
 import {
     UserGroupIcon,
     HomeIcon,
     DocumentDuplicateIcon,
-    TrophyIcon
+    TrophyIcon,
+    TableCellsIcon
   } from '@heroicons/react/24/outline';
-  
+import clsx from 'clsx';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
   // Map of links to display in the side navigation.
   // Depending on the size of the application, this would be stored in a database.
   const links = [
@@ -15,22 +21,31 @@ import {
       icon: UserGroupIcon,
     },
     { name: 'Tournaments', href: '/dashboard/tournaments', icon: TrophyIcon },
+    { name: 'Leagues', href: '/dashboard/leagues', icon: TableCellsIcon },
   ];
   
   export default function NavLinks() {
+
+    const pathname = usePathname();
+
     return (
       <>
         {links.map((link) => {
           const LinkIcon = link.icon;
           return (
-            <a
+            <Link
               key={link.name}
               href={link.href}
-              className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+              className={clsx(
+                "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-green-300 hover:text-green-600 md:flex-none md:justify-start md:p-2 md:px-3",
+              {
+                'bg-green-300 text-green-600':pathname === link.href,
+              })}
+              
             >
               <LinkIcon className="w-6" />
               <p className="hidden md:block">{link.name}</p>
-            </a>
+            </Link>
           );
         })}
       </>
