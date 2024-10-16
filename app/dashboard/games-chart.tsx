6054cf0c0031ff0d26ee5a183e432dfe1fbb95c2
jlarata <arielwdev@gmail.com>
2024-10-16 14:31:45 -0300

    ...

--- app/dashboard/games-chart.tsx
+++ app/dashboard/games-chart.tsx
@@ -63,7 +63,7 @@ export default async function GamesChart(){
         
         <div className="flex items-center pb-2 pt-6">
           <CalendarIcon className="h-5 w-5 text-gray-500" />
-          <h3 className="ml-2 text-sm text-gray-500 ">This year</h3>
+          <h3 className="ml-2 text-sm text-gray-500 ">Last 12 months</h3>
         </div>
       </div>
     </div>
