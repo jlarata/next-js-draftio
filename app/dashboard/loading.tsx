bf215276397493c6285500ec80dfaaee50ceb447
jlarata <arielwdev@gmail.com>
2024-10-18 11:30:04 -0300

    added skeletons to the loading in dashboard

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,3 +1,7 @@
+import { inter } from '@/app/ui/fonts';
+import DashboardSkeleton from '../ui/skeletons';
+
 export default function Loading() {
-    return <div>Loading...</div>
-}
+    return <DashboardSkeleton />
+    }
+
